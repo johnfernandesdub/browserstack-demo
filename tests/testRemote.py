a9c23da190968ed690401839d3a9b3da5eb3b0bd
johnfernandesdub <johnfernandesdub@gmail.com>
2024-06-28 15:42:26 +0100

    Add files via upload

---
+++ tests/testRemote.py
@@ -0,0 +1,84 @@
+import json
+from datetime import datetime 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+
+#Start Timer
+start_time = datetime.now() 
+
+# Load the configuration file
+with open('browserstack_config.json') as config_file:
+    config = json.load(config_file)
+
+# BrowserStack credentials and URL
+USERNAME = config['userName']
+ACCESS_KEY = config['accessKey']
+BSTACK_USERNAME = config['browserstack_user']
+BSTACK_PASSWORD = config['browserstack_password']
+INVITE_URL = config['invite_url']
+BROWSER_STACK_URL = config['brower_stack_url']
+URL = f"https://{USERNAME}:{ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub"
+
+options = ChromeOptions()
+options.set_capability('sessionName', 'BStack Testo')
+
+try:
+    driver = webdriver.Remote(
+        command_executor=URL,
+        options=options)
+
+    driver.get(BROWSER_STACK_URL)
+    driver.maximize_window()    
+    
+    WebDriverWait(driver, 40).until(
+        EC.presence_of_element_located((By.XPATH,"//*[contains(text(),'Sign in')]"))
+    ).click()
+     
+    #Find the username, password and click elements
+    login=driver.find_element(By.ID, "user_email_login");
+    password=driver.find_element(By.ID, "user_password");
+        
+    login.send_keys(BSTACK_USERNAME)
+    password.send_keys(BSTACK_PASSWORD)
+    driver.execute_script("document.querySelector('#user_submit').click();")
+    
+    invite=WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "invite-link"))
+    )
+    
+    assert invite is not None, f"The Invite Team link was not found on BrowserStack web site"
+    
+    inviteUrl=invite.get_property("href")
+    
+    assert len(inviteUrl) != 0, f"The Invite Team link was not found"
+    assert INVITE_URL.lower() == inviteUrl.lower(), f"The Teams URL obtained from BrowserStack web site matches the internal one"
+    
+    print("The URL extracted matched...")
+    
+    #Log out of BrowserStack
+    driver.execute_script("document.querySelector('#account-menu-toggle').click();")
+    signout=WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "sign_out_link"))
+    )
+    signout.click()
+    
+except NoSuchElementException as err:
+    message = 'Exception: ' + str(err.__class__) + str(err.msg)
+    driver.execute_script(
+        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": ' + json.dumps(message) + '}}')
+except Exception as err:
+    message = 'Exception: ' + str(err.__class__) + str(err.msg)
+    driver.execute_script(
+        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": ' + json.dumps(message) + '}}')
+finally:
+    # Stop the driver
+    driver.quit()
+    
+#End time
+end_time = datetime.now()
+time_difference = (end_time - start_time).total_seconds() 
+print("Execution of the test is: ", time_difference, "secs")   
