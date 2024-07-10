04894885fc093574ea1cf3cb5d2a6623774cf036
johnfernandesdub <johnfernandesdub@gmail.com>
2024-07-10 11:57:10 +0100

    Add files via upload

--- tests/testRemote.py
+++ tests/testRemote.py
@@ -19,62 +19,39 @@ with open('browserstack_config.json') as config_file:
 # BrowserStack credentials and URL
 BSTACK_USERNAME = config['browserstack_user']
 BSTACK_PASSWORD = config['browserstack_password']
-USERNAME = BSTACK_USERNAME
+USERNAME = config['username']
 ACCESS_KEY = config['accessKey']
-INVITE_URL = config['invite_url']
-BROWSER_STACK_URL = config['brower_stack_url']
+OLYMPIC_URL = config['olympic_url']
+BROWSER_URL = config['irish_times_url']
 URL = f"https://{USERNAME}:{ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub"
 
 options = ChromeOptions()
-#options.accept_insecure_certs=True
 options.set_capability('sessionName', 'BStack Testo')
 options.set_capability('local', 'True')
 
 try:
-    logger.info("Logging into BrowserStack Web Site...")
+    logger.info("Logging into BrowserStack Test Site...")
     driver = webdriver.Remote(
         command_executor=URL,
         options=options)
 
-    driver.get(BROWSER_STACK_URL)
+    driver.get(BROWSER_URL)
     driver.maximize_window()    
     
-    logger.info("Navigate to the Sign In Page...")
-    WebDriverWait(driver, 40).until(
-        EC.presence_of_element_located((By.XPATH,"//*[contains(text(),'Sign in')]"))
-    ).click()
-     
-    #Find the username, password and click elements
-    logger.info("Enter the Credentials...")
-    login=driver.find_element(By.ID, "user_email_login");
-    password=driver.find_element(By.ID, "user_password");
-        
-    login.send_keys(BSTACK_USERNAME)
-    password.send_keys(BSTACK_PASSWORD)
-    driver.execute_script("document.querySelector('#user_submit').click();")
-    
-    logger.info("Locate the Invite Team Link...")
-    invite=WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "invite-link"))
+    logger.info("Navigate to the Web Site and extract link ...")
+    elementOlympic=WebDriverWait(driver, 100).until(
+        EC.presence_of_element_located((By.XPATH,"//*[@id='main-nav']/div[1]/nav/span[2]/a"))
     )
+           
+    logger.info("Perform Assertion on the link...")
+    assert elementOlympic is not None, f"The Paris Olympic element was on not found on the web site"
     
-    logger.info("Perform Assertion as per the brief...")
-    assert invite is not None, f"The Invite Team link was not found on BrowserStack web site"
+    olympicUrl=elementOlympic.get_property("href")
     
-    inviteUrl=invite.get_property("href")
+    assert len(olympicUrl) != 0, f"The Olympic Url link was not found"
+    assert OLYMPIC_URL.lower() == olympicUrl.lower(), f"The Paris Olympic from the web site does not match the internal one"
     
-    assert len(inviteUrl) != 0, f"The Invite Team link was not found"
-    assert INVITE_URL.lower() == inviteUrl.lower(), f"The Teams URL obtained from BrowserStack web site matches the internal one"
-    
-    logger.info("The URL extracted matched...")
-    
-    #Log out of BrowserStack
-    logger.info("Sign out of BrowserStack web site...")
-    driver.execute_script("document.querySelector('#account-menu-toggle').click();")
-    signout=WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "sign_out_link"))
-    )
-    signout.click()
+    logger.info("The Test is completed...")
     
 except NoSuchElementException as err:
     message = 'Exception: ' + str(err.__class__) + str(err.msg)
